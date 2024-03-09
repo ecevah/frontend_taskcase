c4fd8a596e6590f57c8c3a643a71e402ef1aed8c
Ahmet Ecevit <80712071+ecevah@users.noreply.github.com>
2024-03-09 16:21:41 +0300

    Update shape.js in green box position

--- css_task/src/components/atoms/absolute_photos/shape.js
+++ css_task/src/components/atoms/absolute_photos/shape.js
@@ -9,7 +9,7 @@ const Shape = (props) => {
       >
         <div className="absolute w-[666.28px] h-[217.93px] bg-[#FDE68A] -rotate-45 bottom-[205px] -left-[20px] -z-2" />
         <div className="absolute w-[90px] h-[90px] bg-[rgb(180,83,9,0.75)] rounded-[30px] top-[188.19px] left-[55px] -z-2" />
-        <div className="absolute w-[85px] h-[86px] bg-[rgb(21,128,61,0.75)] rounded-[50px] xl:top-[106.05px] xl:left-[145px] bottom-[111px] left-[145px] z-10" />
+        <div className="absolute w-[85px] h-[86px] bg-[rgb(21,128,61,0.75)] rounded-[50px] min-[1359px]:top-[106.05px] min-[1359px]:left-[145px] bottom-[111px] left-[145px] max-[1359px]:z-10" />
         <div className="absolute w-[59px] h-[60px] bg-[rgb(3,105,161,0.75)] rounded-[50px] top-[43.19px] right-[139px] -z-2" />
         <div className="absolute w-[74px] h-[74px] bg-[rgb(162,28,175,0.75)] rounded-[30px] bottom-[98.05px] right-[181px] -z-2" />
         <div className="absolute md:w-[218px] md:h-[218px] h-[160px] w-[160px] bg-[rgb(190,24,93,0.75)] rounded-[50px] bottom-[33.05px] md:right-0 right-[55px] -z-2" />
